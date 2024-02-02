15a9d527be243b811a5397788ebaed4b60becdc7
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2024-02-02 13:00:00 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+
+
+AIO_FEED_IDS = ["Button led", "Button pump"]
+AIO_USERNAME = "nnthien"
+AIO_KEY = "aio_GwDz64u4llQ4gzZQYCrlBr6UDgtG"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDS:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+sensor_type = 0
+
+while True:
+    counter = counter -1
+    if counter <= 0:
+        counter = 10
+        # TODO
+        print("Random data is publishing.....")
+        if sensor_type == 0:
+            print("Temperature.....")
+            temp = random.randint(10, 20)
+            client.publish("Temperature", temp)
+            sensor_type = 1
+        elif sensor_type == 1:
+            print("Humi.....")
+            humi = random.randint(50, 70)
+            client.publish("Humidity", humi)
+            sensor_type = 2
+        elif sensor_type == 2:
+            print("Light.....")
+            light = random.randint(100, 500)
+            client.publish("Light", light) 
+            sensor_type = 0
+    time.sleep(1)
+    pass
