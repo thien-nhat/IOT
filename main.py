dad926837efcc88f0680d0c520b40175b0cd5ff2
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2024-03-01 12:25:44 +0700

    lab2.2

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sys
 from Adafruit_IO import MQTTClient
 import time
 import random
-
+from uart import *
 
 AIO_FEED_IDS = ["Button led", "Button pump"]
 AIO_USERNAME = "nnthien"
-AIO_KEY = "aio_GwDz64u4llQ4gzZQYCrlBr6UDgtG"
+AIO_KEY = "aio_NFZv38X7ko6uvAIWGeJp7Oc75Yy9"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -34,25 +34,27 @@ counter = 10
 sensor_type = 0
 
 while True:
-    counter = counter -1
-    if counter <= 0:
-        counter = 10
-        # TODO
-        print("Random data is publishing.....")
-        if sensor_type == 0:
-            print("Temperature.....")
-            temp = random.randint(10, 20)
-            client.publish("Temperature", temp)
-            sensor_type = 1
-        elif sensor_type == 1:
-            print("Humi.....")
-            humi = random.randint(50, 70)
-            client.publish("Humidity", humi)
-            sensor_type = 2
-        elif sensor_type == 2:
-            print("Light.....")
-            light = random.randint(100, 500)
-            client.publish("Light", light) 
-            sensor_type = 0
+    # counter = counter -1
+    # if counter <= 0:
+    #     counter = 10
+    #     # TODO
+    #     print("Random data is publishing.....")
+    #     if sensor_type == 0:
+    #         print("Temperature.....")
+    #         temp = random.randint(10, 20)
+    #         client.publish("Temperature", temp)
+    #         sensor_type = 1
+    #     elif sensor_type == 1:
+    #         print("Humi.....")
+    #         humi = random.randint(50, 70)
+    #         client.publish("Humidity", humi)
+    #         sensor_type = 2
+    #     elif sensor_type == 2:
+    #         print("Light.....")
+    #         light = random.randint(100, 500)
+    #         client.publish("Light", light) 
+    #         sensor_type = 0
+    
+    readSerial(client)
     time.sleep(1)
-    pass
+    # pass
